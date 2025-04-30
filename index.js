ac4798fed62f3871a92cb84d546db23ba1dbdd96
muhammedajsalk <ajsalmuhammed747@gmail.com>
2025-04-30 14:15:58 +0530

    buffer program

--- index.js
+++ index.js
@@ -1,16 +1,2 @@
-const fs = require('fs');
-
-// Create a readable stream with encoding
-const readableStream = fs.createReadStream('greet.txt', { encoding: 'utf8' });
-
-readableStream.on('data', (chunk) => {
-    console.log('Received a chunk of data:', chunk);
-});
-
-readableStream.on('end', () => {
-    console.log('Finished reading the file.');
-});
-
-readableStream.on('error', (err) => {
-    console.error('Error reading the file:', err.message);
-});
+const buffer = Buffer.from('Hello, Node.js!');
+console.log(buffer);
