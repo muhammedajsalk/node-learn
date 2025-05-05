0482349fa3135817b491d040fe135c740a6ac26f
muhammedajsalk <ajsalmuhammed747@gmail.com>
2025-05-05 11:21:25 +0530

    thread pool example

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-const buffer = Buffer.from('Hello, Node.js!');
-console.log(buffer);
+const crypto = require("node:crypto");
+const MAX_CALLS = 9;
+const start = Date.now();
+for (let i = 0; i < MAX_CALLS; i++) {
+  crypto.pbkdf2("password", "salt", 100000, 512, "sha512", () => {
+    console.log(`Hash: ${i + 1}`, Date.now() - start);
+  });
+}
